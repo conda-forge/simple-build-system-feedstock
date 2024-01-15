b6da6f50832a0129890d73eb57cb661b08cb4f7d
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2024-01-15 10:46:08 +0100

    add activation scripts

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -9,7 +9,12 @@ sb --init core_val COMPACT
 test -f simplebuild.cfg
 cat simplebuild.cfg
 sb -t
-sbenv sb_core_extdeps --require-disabled NCrystal Numpy matplotlib Geant4 ZLib
-eval "$(sb --env-setup)"
-python3 -c 'import _simple_build_system.envsetup as sbe; sbe.verify_env_already_setup()'
 sb_core_extdeps --require-disabled NCrystal Numpy matplotlib Geant4 ZLib
+python3 -c 'import _simple_build_system.envsetup as sbe; sbe.verify_env_already_setup()'
+eval "$(sb --env-setup)"
+sb_core_extdeps --require-disabled NCrystal Numpy matplotlib Geant4 ZLib
+eval "$(sb --env-unsetup)"
+EC=0
+sb_core_extdeps || EC=$?
+test $EC -ne 0
+sbenv sb_core_extdeps --require-disabled NCrystal Numpy matplotlib Geant4 ZLib
