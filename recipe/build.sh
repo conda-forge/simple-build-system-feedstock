282a9985ab87a3651be7c4837056e6d8c28d5e0c
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2024-02-02 08:42:32 +0100

    fix env var name

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,8 @@ set -eux
 #tag. So we initialise a git repo and add the version tag.
 git init
 git add .
-git commit -m "Source version v${VERSION}"
-git tag v${VERSION}
+git commit -m "Source version v${PKG_VERSION}"
+git tag v${PKG_VERSION}
 python -m pip install --no-deps -vv .
 mkdir -p "${PREFIX}/etc/conda/activate.d"
 mkdir -p "${PREFIX}/etc/conda/deactivate.d"
@@ -13,5 +13,5 @@ cp ./resources/shellrc_snippet.sh \
    "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
 cp ./resources/shellrc_snippet_deactivate.sh \
    "${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}_deactivate.sh"
-echo "TESTING OUR VERSION:"
+echo "TESTING OUR PKG_VERSION (should be ${PKG_VERSION}):"
 python -m pip list | grep simple
